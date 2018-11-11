ef04dd12789c0e6a80db181b4420f3673df4447c
maxim4133 <maxim71515@gmail.com>
2018-11-11 10:52:59 +0300

    Добавлено решение

---
+++ MsdnSpy.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MsdnSpy
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("The bot does not exist, f... you");
+		}
+	}
+}
