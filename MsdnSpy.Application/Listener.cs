400414443bbbe6510f53f9f7b51db7c4ffa26ca9
d4n0n_myself <danon.sibaev@gmail.com>
2018-12-03 19:21:30 +0300

    fix to Listener

--- MsdnSpy.Application/Listener.cs
+++ MsdnSpy.Application/Listener.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Linq;
 using System.Text;
 using MsdnSpy.Domain;
+using Newtonsoft.Json;
 
 namespace MsdnSpy.Application
 {
@@ -36,18 +37,23 @@ namespace MsdnSpy.Application
             while (true)
             {
                 var context1 = _httpListener.GetContext(); // work
-                //var context = await _httpListener.GetContextAsync(); // doesn't work ?   
+                //var context = await _httpListener.GetContextAsync(); // doesn't work ?  
                 var parameterStr = context1.Request.QueryString["query"];
+                Console.WriteLine($"Received request with parameter : {parameterStr}");
                 var queryToXml =
                     "https://raw.githubusercontent.com/dotnet/dotnet-api-docs/master/" +
                     InfoParser.FindXmlFilePath(parameterStr);
                 var msdnLink =
                     Test.GetMsdnUrl(
-                        $"https://social.msdn.microsoft.com/Search/ru-RU?query={parameterStr[1]}&pgArea=header&emptyWatermark=true&ac=4");
+                        $"https://social.msdn.microsoft.com/Search/ru-RU?query={parameterStr}&pgArea=header&emptyWatermark=true&ac=4");
                 var parsedXml = InfoParser.XmlParser(queryToXml);
-                var buffer = Encoding.UTF8.GetBytes(parsedXml["Docs.summary"]).Concat(Encoding.UTF8.GetBytes(msdnLink)).ToArray();
+                parsedXml.Add("msdnLink", msdnLink);
+                //var str = parsedXml.ToString();
+                var stringResult = JsonConvert.SerializeObject(parsedXml);
+                var buffer = Encoding.UTF8.GetBytes(stringResult);
                 using (var outputStream = context1.Response.OutputStream)
                     outputStream.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("Handled.");
             }
         }
     }
--- MsdnSpy.Bot/BotMessageHandler.cs
+++ MsdnSpy.Bot/BotMessageHandler.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
 using MsdnSpy.Core;
 using MsdnSpy.Infrastructure;
+using Newtonsoft.Json;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -40,10 +42,10 @@ namespace MsdnSpy.Bot
 					x.Value ? new InlineKeyboardButton {Text = x.Key, Url = "https://google.com/"} : null).Select(
 					x => new List<InlineKeyboardButton> {x}).ToList();
 				var replyKeyboardMarkup =
-					new InlineKeyboardMarkup(inlineKeyboardButtons);
+					new InlineKeyboardMarkup(inlineKeyboardButtons) { };
 
 				var responseString = SendRequest(args);
-				var answer = $"{args.Message.Text}\r\n\r\n{responseString[0]}\r\n\r\n{responseString[1]}";
+				var answer = $"{args.Message.Text}\r\n\r\n{responseString["Docs.summary"]}\r\n\r\n{responseString["msdnLink"]}";
 				bot.SendTextMessageAsync(chatId, answer, replyMarkup: replyKeyboardMarkup);
 			}
 			catch (Exception e)
@@ -55,16 +57,16 @@ namespace MsdnSpy.Bot
 			Console.WriteLine($"{DateTime.UtcNow}: Handled.");
 		}
 
-		private static string[] SendRequest(MessageEventArgs args)
+		private static IDictionary<string,string> SendRequest(MessageEventArgs args)
 		{
 			var request = WebRequest.Create($"http://localhost:1234/?query={args.Message.Text}");
 			var reader = request.GetResponse().GetResponseStream() ??
 			             throw new ArgumentNullException(nameof(request.GetResponse));
 			var buffer = new byte[1024];
 			reader.Read(buffer, 0, 1024);
-			if (buffer.Length == 0) return new[] {string.Empty};
-			var result = Encoding.ASCII.GetString(buffer.Where(x => x != 0).ToArray()).Split("https");
-			return new[] {result[0], "https" + result[1]};
+			if (buffer.Length == 0) return new Dictionary<string, string>();
+			var result = Encoding.ASCII.GetString(buffer.Where(x => x != 0).ToArray());
+			return JsonConvert.DeserializeObject<IDictionary<string, string>>(result);
 		}
 	}
 }
